76614f976d630f987a7a9a0093fe3519c390a6f4
Vicky1jat <vicky1jat6376831267@gmail.com>
2025-07-02 11:02:38 -0400

    chnages in error handling and deployment

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -7,8 +7,19 @@ def emotion_detector(text_to_analyze):
     myobj = { "raw_document": { "text": text_to_analyze } }
 
     response = requests.post(url, json=myobj, headers=header)
-    formatted_response = json.loads(response.text)
 
+    # Error handling for status code 400 (Bad Request)
+    if response.status_code == 400:
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
+
+    formatted_response = json.loads(response.text)
     emotion_scores = formatted_response['emotionPredictions'][0]['emotion']
 
     anger_score = emotion_scores['anger']
